92d638fdab1b71da2791c55edc42b78fa951ec24
yafaavramovitz <yafa@meorot.co.il>
2025-04-28 13:23:42 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const venom = require('venom-bot');
 const axios = require('axios');
 
-venom
-  .create()
-  .then((client) => start(client))
+venom.create({
+  browserArgs: ['--no-sandbox', '--disable-setuid-sandbox'],
+}).then((client) => start(client))
   .catch((erro) => {
     console.log(erro);
   });
